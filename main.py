30fb5601dd27fddba4cc375e4306c2abc851c47a
fullmers <sarah.fullmer@gmail.com>
2017-12-05 14:57:21 +0100

    create email message for each santa

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 
 import sys
 import csv
+import sendEmail
 
+
+                
+    
 if __name__ == '__main__':
     santas = sys.argv[1]
 
@@ -17,8 +21,19 @@ if __name__ == '__main__':
     randomKeyOrder = list(santaDictionary.keys())
     with open('santa_solution','w') as solutionFile:
         solutionFile = open('santa_solution','w')
-        for key in randomKeyOrder:
+        last = len(randomKeyOrder) -1
+        for i, key in enumerate(randomKeyOrder):
             solutionFile.write(key + '\n') #keep record of santa list, to reveal at end
-    
+            if i == last:
+                santa = santaDictionary[randomKeyOrder[i]]
+                santee = santaDictionary[randomKeyOrder[0]]
+            else:
+                santa = santaDictionary[randomKeyOrder[i]]
+                santee = santaDictionary[randomKeyOrder[i+1]]
+            
+            emailMessage = sendEmail.constructMessage(santa,santee)
+                        
+            print("santa: " + santa + ", santee: " + santee)
+            print(emailMessage)
     print(randomKeyOrder)
     print(list(santaDictionary.values()))
--- sendEmail.py
+++ sendEmail.py
@@ -4,7 +4,22 @@ import authenticationInfo
 login = authenticationInfo.login
 password = authenticationInfo.password
 smtpserver='smtp.gmail.com:587'
-          
+
+SPENDING_LIMIT = "150 NOK"
+
+def constructMessage(santaName, santeeName):
+    greeting = "Dear " + santaName + ",\n\n"
+    body = ("Thank you for participating in this year's Shortcut Secret Santa exchange!"
+            " You are Santa to " + santeeName + ".\n\n"
+            "Kindly remember: there is a " + SPENDING_LIMIT + " on gifts. "
+            "You can alternatively make or bake something for " + santeeName +
+            ", if you think they would like that.\n\n"
+            "Finally, please remember you have until their last day before leaving for the holidays to sneakily deliver your gift ;) "
+            "You can check the 2017 Ferie calendar to find out that date.\n\n\n")
+    closing = "Ho ho ho, \nSanta Sarah"
+    fullmessage = greeting + body + closing
+    return fullmessage
+
 def sendemail(from_addr, to_addr, message):
     server = smtplib.SMTP(smtpserver)
     server.starttls()
@@ -13,5 +28,5 @@ def sendemail(from_addr, to_addr, message):
     server.quit()
     
 
-message = 'test message'
-sendemail(login, 'sarah.fullmer@gmail.com', message)
+#message = 'test message'
+#sendemail(login, 'sarah.fullmer@gmail.com', message)
